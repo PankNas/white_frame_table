08e1a41f7ab0473e00bcbdb6959603bc527f9f6e
a.pankratova <a.pankratova@digitalms.ru>
2024-09-11 20:59:07 +0300

    feat: добавит асинхронное получение данных для таблицы

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import {Table} from 'antd';
+import styles from './styles.module.css';
+import {useEffect, useState} from "react";
+import {getTableData} from "./utils.ts";
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
 function App() {
-  return <div>test</div>
+  const [tableData, setTableData] = useState<any>(null);
+
+  useEffect(() => {
+    const load = async () => {
+      const data = await getTableData();
+      setTableData(data)
+    }
+
+    load();
+  }, []);
+
+  return (
+    <main className={styles.mainPage}>
+      <Table dataSource={tableData} columns={columns}/>
+    </main>
+  )
 }
 
 export default App
